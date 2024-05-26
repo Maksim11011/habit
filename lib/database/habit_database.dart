694de6f88f3b3d5189711de3ad55f46c34648d1d
Maksim11011 <mvbspb@bk.ru>
2024-05-26 23:27:58 +0300

    CRUD functionality

--- lib/database/habit_database.dart
+++ lib/database/habit_database.dart
@@ -43,14 +43,101 @@ class HabitDatabase extends ChangeNotifier {
   */
 
   // List of habbits
+  final List<Habit> currentHabits = [];
 
   // C R E A T E - add a new habbit
+  Future<void> addHabit(String habitName) async {
+    // create a new habit
+    final newHabit = Habit()..name = habitName;
+
+    // save to db
+    await isar.writeTxn(() => isar.habits.put(newHabit));
+
+    // re-read from db
+    readHabits();
+  }
 
   // R E A D - read saved habits from db
+  Future<void> readHabits() async {
+    // fetch all habits from db
+    List<Habit> fetchedHabits = await isar.habits.where().findAll();
+
+    // give to current habbits
+    currentHabits.clear();
+    currentHabits.addAll(fetchedHabits);
+
+    // update UI
+    notifyListeners();
+  }
 
   // U P D A T E - check habit on and off
+  Future<void> updateHabitCompletion(int id, bool isCompleted) async {
+    // find the spicific habit
+    final habit = await isar.habits.get(id);
+
+    // update completed status
+    if (habit != null) {
+      await isar.writeTxn(() async {
+        // if habit is completed -> add the current date to the completedDayslist
+        if (isCompleted && !habit.comletedDays.contains(DateTime.now())) {
+          // today
+          final today = DateTime.now();
+
+          // add the current date if it'snot already in the list
+          habit.comletedDays.add(
+            DateTime(
+              today.year,
+              today.month,
+              today.day,
+            ),
+          );
+        }
+
+        // if habit is NOT completed -> remove the current date from the list
+        else {
+          // remove the current date if the habit is marked as not completed
+          habit.comletedDays.removeWhere(
+            (date) =>
+                date.year == DateTime.now().year &&
+                date.month == DateTime.now().month &&
+                date.day == DateTime.now().day,
+          );
+        }
+        // save the updated habits back to the db
+        await isar.habits.put(habit);
+      });
+    }
+    // re-read from db
+    readHabits();
+  }
 
   // U P D A T E - edit habit name
+  Future<void> updateHabitName(int id, String newName) async {
+    // find the spicific habit
+    final habit = await isar.habits.get(id);
+
+    // update habit name
+    if (habit != null) {
+      // update name
+      await isar.writeTxn(() async {
+        habit.name = newName;
+        // save updated habit back to the db
+        await isar.habits.put(habit);
+      });
+    }
+
+    // re-read from db
+    readHabits();
+  }
 
   // D E L E T E - delete habit
+  Future<void> deletedHabit(int id) async {
+    // perform the delete
+    await isar.writeTxn(() async {
+      await isar.habits.delete(id);
+    });
+
+    // re-read from db
+    readHabits();
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,14 @@ void main() async {
   await HabitDatabase.initialize();
   await HabitDatabase().saveFirstLaunchDate();
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => ThemeProvider(),
+    MultiProvider(
+      providers: [
+        // habit provider
+        ChangeNotifierProvider(create: (context) => HabitDatabase()),
+
+        // theme provider
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+      ],
       child: const MyApp(),
     ),
   );
