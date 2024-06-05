d4798cb13e4a9c8f8b59c5fcb3d7debfb896795e
Maksim11011 <mvbspb@bk.ru>
2024-06-05 22:56:07 +0300

    remove error, add checkbox

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:habit/components/my_drawer.dart';
+import 'package:habit/components/my_habit_tile.dart';
+import 'package:habit/database/habit_database.dart';
+import 'package:habit/models/habit.dart';
+import 'package:habit/util/habit_util.dart';
 import 'package:provider/provider.dart';
-import '../theme/theme_provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,20 +15,111 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      drawer: Drawer(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        child: Center(
-          child: CupertinoSwitch(
-            value: Provider.of<ThemeProvider>(context).isDarkMode,
-            onChanged: (value) =>
-                Provider.of<ThemeProvider>(context, listen: false)
-                    .toggleTheme(),
+  void initState() {
+    // read existing habits on app startup
+    Provider.of<HabitDatabase>(context, listen: false).readHabits();
+
+    super.initState();
+  }
+
+  // text controller
+  final TextEditingController textController = TextEditingController();
+
+  // create new habit
+  void createNewHabit() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+          decoration: const InputDecoration(
+            hintText: 'Введите название...',
           ),
         ),
+        actions: [
+          // save button
+          MaterialButton(
+            onPressed: () {
+              // get the new habit name
+              String newHabitName = textController.text;
+
+              // save to db
+              context.read<HabitDatabase>().addHabit(newHabitName);
+
+              // pop box
+              Navigator.pop(context);
+
+              // crear controller
+              textController.clear();
+            },
+            child: const Text('Сохранить'),
+          ),
+
+          // cancel button
+          MaterialButton(onPressed: () {
+            // pop box
+            Navigator.pop(context);
+
+            //clear controller
+            textController.clear();
+          })
+        ],
       ),
+    );
+  }
+
+  // check habit on & off
+  void checkHabitOnOff(bool? value, Habit habit) {
+    // update habit completion status
+    if (value != null) {
+      context.read<HabitDatabase>().updateHabitCompletion(habit.id, value);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(),
+      drawer: const MyDrawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewHabit,
+        elevation: 1,
+        backgroundColor: Theme.of(context).colorScheme.tertiary,
+        child: const Icon(
+          Icons.add,
+          color: Colors.black,
+        ),
+      ),
+      body: _buildHabitList(),
+    );
+  }
+
+  // build habit list
+  Widget _buildHabitList() {
+    // habit db
+    final habitDatabase = context.watch<HabitDatabase>();
+
+    // current habits
+    List<Habit> currentHabits = habitDatabase.currentHabits;
+
+    // return list of habits UI
+    return ListView.builder(
+      itemCount: currentHabits.length,
+      itemBuilder: (context, index) {
+        // get each individual habit
+        final habit = currentHabits[index];
+
+        // check if the habits is completed today
+        bool isCompletedToday = isHabitCompletedToday(habit.comletedDays);
+
+        // return habit tile UI
+        return MyHabitTile(
+          isCompleted: isCompletedToday,
+          text: habit.name,
+          onChanged: (value) => checkHabitOnOff(value, habit),
+        );
+      },
     );
   }
 }
